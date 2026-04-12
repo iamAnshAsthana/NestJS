c9d8d794d444906f198b906de03e52813cc5596b
Ansh <anshasthana121@gmail.com>
2026-04-12 20:23:30 +0530

    NestJS DTO & Services Creation

--- backend/src/category/category.module.ts
+++ backend/src/category/category.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
+import { Category } from './category.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoryController],
   providers: [CategoryService]
 })
--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Category } from './category.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+    constructor(@InjectRepository(Category) private repo:Repository<Category>) {}
+}
