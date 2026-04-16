cbf6cb6e4d5b1d3da331494d166c3ce29e20a2dd
Ansh <anshasthana121@gmail.com>
2026-04-16 16:45:10 +0530

    Fixed

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -4,7 +4,7 @@ import { Category } from './category/category.entity';
 export const AppDataSource = new DataSource({
   type: 'mysql',
   username: 'root',
-  password: '@ansh.0603',
+  password: '',
   database: 'nestjs_mern',
   port: 3306,
   host: 'localhost',
